cc37c1941cf295a535e41a19458b4e473c979ac3
Yamataw <maxime.henri@ynov.com>
2023-01-06 15:58:57 +0100

    404/Home

---
+++ src/components/HomePage.vue
@@ -0,0 +1,8 @@
+<template>
+  <router-view />
+
+  <div class="app">
+    <header><h1>Mon site web</h1></header>
+    <footer>Made with Vue</footer>
+  </div>
+</template>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1 class="text-3xl text-gray-700 font-bold mb-5">404 Not Found</h1>
+</template>
+
