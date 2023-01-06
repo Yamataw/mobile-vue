c9e29206eebfe8540116f1f012b4df41fbfa95a1
Yamataw <maxime.henri@ynov.com>
2023-01-06 11:18:27 +0100

    Tailwind on

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+<style>
+  @import '../public/style.css';
+</style>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+/*import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
   }
-}
+}*/
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
